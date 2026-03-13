df1d7a6a3cebfab6f64fe9338be146877d10cd65
Riccardo Cardia <cardiariccardo94@gmail.com>
2026-03-13 11:34:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">Welcome to React</h1>
+      <label htmlFor="search">search</label>
+      <input id="search" type="text" className="input" />
+    </>
+  );
+}
+
+export default App;
