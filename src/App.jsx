5371f3061ee5c7beb64a6b8804cdf7aa0737408c
Riccardo Cardia <cardiariccardo94@gmail.com>
2026-03-18 16:13:48 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import "./App.css";
-import Navbar from "./Components/Navbar";
-
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import Layout from "./Layout/Layout";
+import Posts from "./Components/Posts";
+import PostDetail from "./Components/PostDetail";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<App />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/:id" element={<PostDetail/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
