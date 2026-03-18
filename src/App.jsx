7806816d671236eb81b62ff88327b15b5e285a69
Riccardo Cardia <cardiariccardo94@gmail.com>
2026-03-18 16:05:44 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,15 @@ import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <>
-     
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/posts/:id" element={<PostDetail/>} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Routes, Route, BrowserRouter } from "react-router";
-import Layout from "./Layout/Layout.jsx";
-import Posts from "./Components/Posts.jsx";
-import PostDetail from "./Components/PostDetail.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<App />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/:id" element={<PostDetail/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
-);
+
+createRoot(document.getElementById("root")).render(<App />);
